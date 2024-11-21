f887529c03629f9f7f11e3cc2461c381fc5ff751
yhx <2045399856@qq.com>
2024-11-22 00:39:29 +0800

    ci:update package-lock.json for dependency updates

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
-  splitting: false,
-  sourcemap: false,
-  minify: true, // 启用压缩
-});
